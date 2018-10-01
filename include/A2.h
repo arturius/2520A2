4695b9181c620700fb9cc7675b2e00b9064a283c
Arthur Anderson <jander21@uoguelph.ca>
2018-10-01 16:08:30 -0400

    intial commit

---
+++ include/A2.h
@@ -0,0 +1,16 @@
+/*  A2.h
+    Header File for : CIS 2520 Assignment 2.
+    By: James Arthur Anderson
+    Student ID #: 1013076
+    Date: Oct. 14, 2018
+*/
+/*including of standard libraries*/
+#include<stdio.h>
+
+#ifndef A2_H
+#define A2_H
+
+
+#endif
+
+  
---
+++ src/q1.c
@@ -0,0 +1,13 @@
+/*  q1.c
+    Main Function file for CIS 2520 Assignment 2 question 1.
+    By: James Arthur Anderson
+    Student ID #: 1013076
+    Date: Oct. 14, 2018
+*/
+#include"A2.h"
+
+int main(){
+    
+    return 0;
+}
+
